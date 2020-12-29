108e1d576fab0d4194226590e186c5362a1d6e20
Hasan Aktas <hasanaktas95@gmail.com>
2020-12-29 09:15:15 +0300

    app name change

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
+import React, {FC} from 'react';
 import Router from 'router';
 import {LogBox} from 'react-native';
 
 LogBox.ignoreLogs(['Setting a timer']);
 
-const App = () => {
+const App: FC = () => {
   return <Router />;
 };
 
--- android/app/src/main/java/com/ardenlinefirebasetypescript/MainActivity.java
+++ android/app/src/main/java/com/ardenlinefirebasetypescript/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ardenlinefirebasetypescript;
+package com.lam;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ardenlineFirebaseTypescript";
+    return "lam";
   }
 }
